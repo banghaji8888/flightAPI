6b188f39b0a42907b15ea8081de88e6476ff1d8c
Hendro Saputra <hendro863@gmail.com>
2019-01-07 04:12:56 +0000

    add

--- switching_system.sql
+++ switching_system.sql
@@ -83,7 +83,7 @@ CREATE TABLE `city` (
   `province_id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=501 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,6 +92,7 @@ CREATE TABLE `city` (
 
 LOCK TABLES `city` WRITE;
 /*!40000 ALTER TABLE `city` DISABLE KEYS */;
+INSERT INTO `city` VALUES (1,1,'Kab. Simeulue'),(2,1,'Kab. Aceh Singkil'),(3,1,'Kab. Aceh Selatan'),(4,1,'Kab. Aceh Tenggara'),(5,1,'Kab. Aceh Timur'),(6,1,'Kab. Aceh Tengah'),(7,1,'Kab. Aceh Barat'),(8,1,'Kab. Aceh Besar'),(9,1,'Kab. Pidie'),(10,1,'Kab. Bireuen'),(11,1,'Kab. Aceh Utara'),(12,1,'Kab. Aceh Barat Daya'),(13,1,'Kab. Gayo Lues'),(14,1,'Kab. Aceh Tamiang'),(15,1,'Kab. Nagan Raya'),(16,1,'Kab. Aceh Jaya'),(17,1,'Kab. Bener Meriah'),(18,1,'Kab. Pidie Jaya'),(19,1,'Kota Banda Aceh'),(20,1,'Kota Sabang'),(21,1,'Kota Langsa'),(22,1,'Kota Lhokseumawe'),(23,1,'Kota Subulussalam'),(24,2,'Kab. Nias'),(25,2,'Kab. Mandailing Natal'),(26,2,'Kab. Tapanuli Selatan'),(27,2,'Kab. Tapanuli Tengah'),(28,2,'Kab. Tapanuli Utara'),(29,2,'Kab. Toba Samosir'),(30,2,'Kab. Labuhan Batu'),(31,2,'Kab. Asahan'),(32,2,'Kab. Simalungun'),(33,2,'Kab. Dairi'),(34,2,'Kab. Karo'),(35,2,'Kab. Deli Serdang'),(36,2,'Kab. Langkat'),(37,2,'Kab. Nias Selatan'),(38,2,'Kab. Humbang Hasundutan'),(39,2,'Kab. Pakpak Bharat'),(40,2,'Kab. Samosir'),(41,2,'Kab. Serdang Bedagai'),(42,2,'Kab. Batu Bara'),(43,2,'Kab. Padang Lawas Utara'),(44,2,'Kab. Padang Lawas'),(45,2,'Kab. Labuhan Batu Selatan'),(46,2,'Kab. Labuhan Batu Utara'),(47,2,'Kab. Nias Utara'),(48,2,'Kab. Nias Barat'),(49,2,'Kota Sibolga'),(50,2,'Kota Tanjung Balai'),(51,2,'Kota Pematang Siantar'),(52,2,'Kota Tebing Tinggi'),(53,2,'Kota Medan'),(54,2,'Kota Binjai'),(55,2,'Kota Padangsidimpuan'),(56,2,'Kota Gunungsitoli'),(57,3,'Kab. Kepulauan Mentawai'),(58,3,'Kab. Pesisir Selatan'),(59,3,'Kab. Solok'),(60,3,'Kab. Sijunjung'),(61,3,'Kab. Tanah Datar'),(62,3,'Kab. Padang Pariaman'),(63,3,'Kab. Agam'),(64,3,'Kab. Lima Puluh Kota'),(65,3,'Kab. Pasaman'),(66,3,'Kab. Solok Selatan'),(67,3,'Kab. Dharmasraya'),(68,3,'Kab. Pasaman Barat'),(69,3,'Kota Padang'),(70,3,'Kota Solok'),(71,3,'Kota Sawah Lunto'),(72,3,'Kota Padang Panjang'),(73,3,'Kota Bukittinggi'),(74,3,'Kota Payakumbuh'),(75,3,'Kota Pariaman'),(76,4,'Kab. Kuantan Singingi'),(77,4,'Kab. Indragiri Hulu'),(78,4,'Kab. Indragiri Hilir'),(79,4,'Kab. Pelalawan'),(80,4,'Kab. S I A K'),(81,4,'Kab. Kampar'),(82,4,'Kab. Rokan Hulu'),(83,4,'Kab. Bengkalis'),(84,4,'Kab. Rokan Hilir'),(85,4,'Kab. Kepulauan Meranti'),(86,4,'Kota Pekanbaru'),(87,4,'Kota D U M A I'),(88,5,'Kab. Kerinci'),(89,5,'Kab. Merangin'),(90,5,'Kab. Sarolangun'),(91,5,'Kab. Batang Hari'),(92,5,'Kab. Muaro Jambi'),(93,5,'Kab. Tanjung Jabung Timur'),(94,5,'Kab. Tanjung Jabung Barat'),(95,5,'Kab. Tebo'),(96,5,'Kab. Bungo'),(97,5,'Kota Jambi'),(98,5,'Kota Sungai Penuh'),(99,6,'Kab. Ogan Komering Ulu'),(100,6,'Kab. Ogan Komering Ilir'),(101,6,'Kab. Muara Enim'),(102,6,'Kab. Lahat'),(103,6,'Kab. Musi Rawas'),(104,6,'Kab. Musi Banyuasin'),(105,6,'Kab. Banyu Asin'),(106,6,'Kab. Ogan Komering Ulu Selatan'),(107,6,'Kab. Ogan Komering Ulu Timur'),(108,6,'Kab. Ogan Ilir'),(109,6,'Kab. Empat Lawang'),(110,6,'Kota Palembang'),(111,6,'Kota Prabumulih'),(112,6,'Kota Pagar Alam'),(113,6,'Kota Lubuklinggau'),(114,7,'Kab. Bengkulu Selatan'),(115,7,'Kab. Rejang Lebong'),(116,7,'Kab. Bengkulu Utara'),(117,7,'Kab. Kaur'),(118,7,'Kab. Seluma'),(119,7,'Kab. Mukomuko'),(120,7,'Kab. Lebong'),(121,7,'Kab. Kepahiang'),(122,7,'Kab. Bengkulu Tengah'),(123,7,'Kota Bengkulu'),(124,8,'Kab. Lampung Barat'),(125,8,'Kab. Tanggamus'),(126,8,'Kab. Lampung Selatan'),(127,8,'Kab. Lampung Timur'),(128,8,'Kab. Lampung Tengah'),(129,8,'Kab. Lampung Utara'),(130,8,'Kab. Way Kanan'),(131,8,'Kab. Tulangbawang'),(132,8,'Kab. Pesawaran'),(133,8,'Kab. Pringsewu'),(134,8,'Kab. Mesuji'),(135,8,'Kab. Tulang Bawang Barat'),(136,8,'Kab. Pesisir Barat'),(137,8,'Kota Bandar Lampung'),(138,8,'Kota Metro'),(139,9,'Kab. Bangka'),(140,9,'Kab. Belitung'),(141,9,'Kab. Bangka Barat'),(142,9,'Kab. Bangka Tengah'),(143,9,'Kab. Bangka Selatan'),(144,9,'Kab. Belitung Timur'),(145,9,'Kota Pangkal Pinang'),(146,10,'Kab. Karimun'),(147,10,'Kab. Bintan'),(148,10,'Kab. Natuna'),(149,10,'Kab. Lingga'),(150,10,'Kab. Kepulauan Anambas'),(151,10,'Kota B A T A M'),(152,10,'Kota Tanjung Pinang'),(153,11,'Kab. Kepulauan Seribu'),(154,11,'Kota Jakarta Selatan'),(155,11,'Kota Jakarta Timur'),(156,11,'Kota Jakarta Pusat'),(157,11,'Kota Jakarta Barat'),(158,11,'Kota Jakarta Utara'),(159,12,'Kab. Bogor'),(160,12,'Kab. Sukabumi'),(161,12,'Kab. Cianjur'),(162,12,'Kab. Bandung'),(163,12,'Kab. Garut'),(164,12,'Kab. Tasikmalaya'),(165,12,'Kab. Ciamis'),(166,12,'Kab. Kuningan'),(167,12,'Kab. Cirebon'),(168,12,'Kab. Majalengka'),(169,12,'Kab. Sumedang'),(170,12,'Kab. Indramayu'),(171,12,'Kab. Subang'),(172,12,'Kab. Purwakarta'),(173,12,'Kab. Karawang'),(174,12,'Kab. Bekasi'),(175,12,'Kab. Bandung Barat'),(176,12,'Kab. Pangandaran'),(177,12,'Kota Bogor'),(178,12,'Kota Sukabumi'),(179,12,'Kota Bandung'),(180,12,'Kota Cirebon'),(181,12,'Kota Bekasi'),(182,12,'Kota Depok'),(183,12,'Kota Cimahi'),(184,12,'Kota Tasikmalaya'),(185,12,'Kota Banjar'),(186,13,'Kab. Cilacap'),(187,13,'Kab. Banyumas'),(188,13,'Kab. Purbalingga'),(189,13,'Kab. Banjarnegara'),(190,13,'Kab. Kebumen'),(191,13,'Kab. Purworejo'),(192,13,'Kab. Wonosobo'),(193,13,'Kab. Magelang'),(194,13,'Kab. Boyolali'),(195,13,'Kab. Klaten'),(196,13,'Kab. Sukoharjo'),(197,13,'Kab. Wonogiri'),(198,13,'Kab. Karanganyar'),(199,13,'Kab. Sragen'),(200,13,'Kab. Grobogan'),(201,13,'Kab. Blora'),(202,13,'Kab. Rembang'),(203,13,'Kab. Pati'),(204,13,'Kab. Kudus'),(205,13,'Kab. Jepara'),(206,13,'Kab. Demak'),(207,13,'Kab. Semarang'),(208,13,'Kab. Temanggung'),(209,13,'Kab. Kendal'),(210,13,'Kab. Batang'),(211,13,'Kab. Pekalongan'),(212,13,'Kab. Pemalang'),(213,13,'Kab. Tegal'),(214,13,'Kab. Brebes'),(215,13,'Kota Magelang'),(216,13,'Kota Surakarta'),(217,13,'Kota Salatiga'),(218,13,'Kota Semarang'),(219,13,'Kota Pekalongan'),(220,13,'Kota Tegal'),(221,14,'Kab. Kulon Progo'),(222,14,'Kab. Bantul'),(223,14,'Kab. Gunung Kidul'),(224,14,'Kab. Sleman'),(225,14,'Kota Yogyakarta'),(226,15,'Kab. Pacitan'),(227,15,'Kab. Ponorogo'),(228,15,'Kab. Trenggalek'),(229,15,'Kab. Tulungagung'),(230,15,'Kab. Blitar'),(231,15,'Kab. Kediri'),(232,15,'Kab. Malang'),(233,15,'Kab. Lumajang'),(234,15,'Kab. Jember'),(235,15,'Kab. Banyuwangi'),(236,15,'Kab. Bondowoso'),(237,15,'Kab. Situbondo'),(238,15,'Kab. Probolinggo'),(239,15,'Kab. Pasuruan'),(240,15,'Kab. Sidoarjo'),(241,15,'Kab. Mojokerto'),(242,15,'Kab. Jombang'),(243,15,'Kab. Nganjuk'),(244,15,'Kab. Madiun'),(245,15,'Kab. Magetan'),(246,15,'Kab. Ngawi'),(247,15,'Kab. Bojonegoro'),(248,15,'Kab. Tuban'),(249,15,'Kab. Lamongan'),(250,15,'Kab. Gresik'),(251,15,'Kab. Bangkalan'),(252,15,'Kab. Sampang'),(253,15,'Kab. Pamekasan'),(254,15,'Kab. Sumenep'),(255,15,'Kota Kediri'),(256,15,'Kota Blitar'),(257,15,'Kota Malang'),(258,15,'Kota Probolinggo'),(259,15,'Kota Pasuruan'),(260,15,'Kota Mojokerto'),(261,15,'Kota Madiun'),(262,15,'Kota Surabaya'),(263,15,'Kota Batu'),(264,16,'Kab. Pandeglang'),(265,16,'Kab. Lebak'),(266,16,'Kab. Tangerang'),(267,16,'Kab. Serang'),(268,16,'Kota Tangerang'),(269,16,'Kota Cilegon'),(270,16,'Kota Serang'),(271,16,'Kota Tangerang Selatan'),(272,17,'Kab. Jembrana'),(273,17,'Kab. Tabanan'),(274,17,'Kab. Badung'),(275,17,'Kab. Gianyar'),(276,17,'Kab. Klungkung'),(277,17,'Kab. Bangli'),(278,17,'Kab. Karang Asem'),(279,17,'Kab. Buleleng'),(280,17,'Kota Denpasar'),(281,18,'Kab. Lombok Barat'),(282,18,'Kab. Lombok Tengah'),(283,18,'Kab. Lombok Timur'),(284,18,'Kab. Sumbawa'),(285,18,'Kab. Dompu'),(286,18,'Kab. Bima'),(287,18,'Kab. Sumbawa Barat'),(288,18,'Kab. Lombok Utara'),(289,18,'Kota Mataram'),(290,18,'Kota Bima'),(291,19,'Kab. Sumba Barat'),(292,19,'Kab. Sumba Timur'),(293,19,'Kab. Kupang'),(294,19,'Kab. Timor Tengah Selatan'),(295,19,'Kab. Timor Tengah Utara'),(296,19,'Kab. Belu'),(297,19,'Kab. Belu'),(298,19,'Kab. Alor'),(299,19,'Kab. Lembata'),(300,19,'Kab. Flores Timur'),(301,19,'Kab. Sikka'),(302,19,'Kab. Ende'),(303,19,'Kab. Ngada'),(304,19,'Kab. Manggarai'),(305,19,'Kab. Rote Ndao'),(306,19,'Kab. Manggarai Barat'),(307,19,'Kab. Sumba Tengah'),(308,19,'Kab. Sumba Barat Daya'),(309,19,'Kab. Nagekeo'),(310,19,'Kab. Manggarai Timur'),(311,19,'Kab. Sabu Raijua'),(312,19,'Kota Kupang'),(313,20,'Kab. Sambas'),(314,20,'Kab. Bengkayang'),(315,20,'Kab. Landak'),(316,20,'Kab. Pontianak'),(317,20,'Kab. Sanggau'),(318,20,'Kab. Ketapang'),(319,20,'Kab. Sintang'),(320,20,'Kab. Kapuas Hulu'),(321,20,'Kab. Sekadau'),(322,20,'Kab. Melawi'),(323,20,'Kab. Kayong Utara'),(324,20,'Kab. Kubu Raya'),(325,20,'Kota Pontianak'),(326,20,'Kota Singkawang'),(327,21,'Kab. Kotawaringin Barat'),(328,21,'Kab. Kotawaringin Timur'),(329,21,'Kab. Kapuas'),(330,21,'Kab. Barito Selatan'),(331,21,'Kab. Barito Utara'),(332,21,'Kab. Sukamara'),(333,21,'Kab. Lamandau'),(334,21,'Kab. Seruyan'),(335,21,'Kab. Katingan'),(336,21,'Kab. Pulang Pisau'),(337,21,'Kab. Gunung Mas'),(338,21,'Kab. Barito Timur'),(339,21,'Kab. Murung Raya'),(340,21,'Kota Palangka Raya'),(341,22,'Kab. Tanah Laut'),(342,22,'Kab. Kota Baru'),(343,22,'Kab. Banjar'),(344,22,'Kab. Barito Kuala'),(345,22,'Kab. Tapin'),(346,22,'Kab. Hulu Sungai Selatan'),(347,22,'Kab. Hulu Sungai Tengah'),(348,22,'Kab. Hulu Sungai Utara'),(349,22,'Kab. Tabalong'),(350,22,'Kab. Tanah Bumbu'),(351,22,'Kab. Balangan'),(352,22,'Kota Banjarmasin'),(353,22,'Kota Banjar Baru'),(354,23,'Kab. Paser'),(355,23,'Kab. Kutai Barat'),(356,23,'Kab. Kutai Kartanegara'),(357,23,'Kab. Kutai Timur'),(358,23,'Kab. Berau'),(359,23,'Kab. Penajam Paser Utara'),(360,23,'Kota Balikpapan'),(361,23,'Kota Samarinda'),(362,23,'Kota Bontang'),(363,24,'Kab. Malinau'),(364,24,'Kab. Bulungan'),(365,24,'Kab. Tana Tidung'),(366,24,'Kab. Nunukan'),(367,24,'Kota Tarakan'),(368,25,'Kab. Bolaang Mongondow'),(369,25,'Kab. Minahasa'),(370,25,'Kab. Kepulauan Sangihe'),(371,25,'Kab. Kepulauan Talaud'),(372,25,'Kab. Minahasa Selatan'),(373,25,'Kab. Minahasa Utara'),(374,25,'Kab. Bolaang Mongondow Utara'),(375,25,'Kab. Siau Tagulandang Biaro'),(376,25,'Kab. Minahasa Tenggara'),(377,25,'Kab. Bolaang Mongondow Selatan'),(378,25,'Kab. Bolaang Mongondow Timur'),(379,25,'Kota Manado'),(380,25,'Kota Bitung'),(381,25,'Kota Tomohon'),(382,25,'Kota Kotamobagu'),(383,26,'Kab. Banggai Kepulauan'),(384,26,'Kab. Banggai'),(385,26,'Kab. Morowali'),(386,26,'Kab. Poso'),(387,26,'Kab. Donggala'),(388,26,'Kab. Toli-toli'),(389,26,'Kab. Buol'),(390,26,'Kab. Parigi Moutong'),(391,26,'Kab. Tojo Una-una'),(392,26,'Kab. Sigi'),(393,26,'Kota Palu'),(394,27,'Kab. Kepulauan Selayar'),(395,27,'Kab. Bulukumba'),(396,27,'Kab. Bantaeng'),(397,27,'Kab. Jeneponto'),(398,27,'Kab. Takalar'),(399,27,'Kab. Gowa'),(400,27,'Kab. Sinjai'),(401,27,'Kab. Maros'),(402,27,'Kab. Pangkajene Dan Kepulauan'),(403,27,'Kab. Barru'),(404,27,'Kab. Bone'),(405,27,'Kab. Soppeng'),(406,27,'Kab. Wajo'),(407,27,'Kab. Sidenreng Rappang'),(408,27,'Kab. Pinrang'),(409,27,'Kab. Enrekang'),(410,27,'Kab. Luwu'),(411,27,'Kab. Tana Toraja'),(412,27,'Kab. Luwu Utara'),(413,27,'Kab. Luwu Timur'),(414,27,'Kab. Toraja Utara'),(415,27,'Kota Makassar'),(416,27,'Kota Parepare'),(417,27,'Kota Palopo'),(418,28,'Kab. Buton'),(419,28,'Kab. Muna'),(420,28,'Kab. Konawe'),(421,28,'Kab. Kolaka'),(422,28,'Kab. Konawe Selatan'),(423,28,'Kab. Bombana'),(424,28,'Kab. Wakatobi'),(425,28,'Kab. Kolaka Utara'),(426,28,'Kab. Buton Utara'),(427,28,'Kab. Konawe Utara'),(428,28,'Kota Kendari'),(429,28,'Kota Baubau'),(430,29,'Kab. Boalemo'),(431,29,'Kab. Gorontalo'),(432,29,'Kab. Pohuwato'),(433,29,'Kab. Bone Bolango'),(434,29,'Kab. Gorontalo Utara'),(435,29,'Kota Gorontalo'),(436,30,'Kab. Majene'),(437,30,'Kab. Polewali Mandar'),(438,30,'Kab. Mamasa'),(439,30,'Kab. Mamuju'),(440,30,'Kab. Mamuju Utara'),(441,31,'Kab. Maluku Tenggara Barat'),(442,31,'Kab. Maluku Tenggara'),(443,31,'Kab. Maluku Tengah'),(444,31,'Kab. Buru'),(445,31,'Kab. Kepulauan Aru'),(446,31,'Kab. Seram Bagian Barat'),(447,31,'Kab. Seram Bagian Timur'),(448,31,'Kab. Maluku Barat Daya'),(449,31,'Kab. Buru Selatan'),(450,31,'Kota Ambon'),(451,31,'Kota Tual'),(452,32,'Kab. Halmahera Barat'),(453,32,'Kab. Halmahera Tengah'),(454,32,'Kab. Kepulauan Sula'),(455,32,'Kab. Halmahera Selatan'),(456,32,'Kab. Halmahera Utara'),(457,32,'Kab. Halmahera Timur'),(458,32,'Kab. Pulau Morotai'),(459,32,'Kota Ternate'),(460,32,'Kota Tidore Kepulauan'),(461,33,'Kab. Fakfak'),(462,33,'Kab. Kaimana'),(463,33,'Kab. Teluk Wondama'),(464,33,'Kab. Teluk Bintuni'),(465,33,'Kab. Manokwari'),(466,33,'Kab. Sorong Selatan'),(467,33,'Kab. Sorong'),(468,33,'Kab. Raja Ampat'),(469,33,'Kab. Tambrauw'),(470,33,'Kab. Maybrat'),(471,33,'Kota Sorong'),(472,34,'Kab. Merauke'),(473,34,'Kab. Jayawijaya'),(474,34,'Kab. Jayapura'),(475,34,'Kab. Nabire'),(476,34,'Kab. Kepulauan Yapen'),(477,34,'Kab. Biak Numfor'),(478,34,'Kab. Paniai'),(479,34,'Kab. Puncak Jaya'),(480,34,'Kab. Mimika'),(481,34,'Kab. Boven Digoel'),(482,34,'Kab. Mappi'),(483,34,'Kab. Asmat'),(484,34,'Kab. Yahukimo'),(485,34,'Kab. Pegunungan Bintang'),(486,34,'Kab. Tolikara'),(487,34,'Kab. Sarmi'),(488,34,'Kab. Keerom'),(489,34,'Kab. Waropen'),(490,34,'Kab. Supiori'),(491,34,'Kab. Mamberamo Raya'),(492,34,'Kab. Nduga'),(493,34,'Kab. Lanny Jaya'),(494,34,'Kab. Mamberamo Tengah'),(495,34,'Kab. Yalimo'),(496,34,'Kab. Puncak'),(497,34,'Kab. Dogiyai'),(498,34,'Kab. Intan Jaya'),(499,34,'Kab. Deiyai'),(500,34,'Kota Jayapura');
 /*!40000 ALTER TABLE `city` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -131,21 +132,24 @@ DROP TABLE IF EXISTS `merchant`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `merchant` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
   `phone` varchar(20) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `address` text NOT NULL,
-  `city_id` int(11) NOT NULL,
-  `province_id` int(11) NOT NULL,
-  `latitude` text NOT NULL,
-  `longitude` text NOT NULL,
-  `balance` bigint(20) NOT NULL,
+  `address` text DEFAULT NULL,
+  `city_id` int(11) DEFAULT NULL,
+  `province_id` int(11) DEFAULT NULL,
+  `latitude` text DEFAULT NULL,
+  `longitude` text DEFAULT NULL,
+  `balance` bigint(20) DEFAULT NULL,
   `minimal_balance` bigint(20) NOT NULL DEFAULT 1000000,
   `active` enum('verifying','active','inactive','verified','reject','removed') NOT NULL,
-  `created_date` int(11) NOT NULL,
-  `last_active` int(11) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `last_active` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `level` enum('basic','pro','enterprise') NOT NULL DEFAULT 'basic',
-  `type` enum('partner','retail','user') NOT NULL,
+  `type` enum('partner','retail','user') DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `api_username` varchar(20) DEFAULT NULL,
+  `api_password` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -192,13 +196,14 @@ DROP TABLE IF EXISTS `message_log`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `message_log` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `cycle` bigint(20) NOT NULL,
   `sender` varchar(100) NOT NULL,
   `receiver` varchar(100) NOT NULL,
   `content` text NOT NULL,
   `stats` varchar(50) NOT NULL,
-  `created_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -209,6 +214,28 @@ CREATE TABLE `message_log` (
 LOCK TABLES `message_log` WRITE;
 /*!40000 ALTER TABLE `message_log` DISABLE KEYS */;
 /*!40000 ALTER TABLE `message_log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message_sequence`
+--
+
+DROP TABLE IF EXISTS `message_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message_sequence` (
+  `id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message_sequence`
+--
+
+LOCK TABLES `message_sequence` WRITE;
+/*!40000 ALTER TABLE `message_sequence` DISABLE KEYS */;
+INSERT INTO `message_sequence` VALUES (0);
+/*!40000 ALTER TABLE `message_sequence` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -282,7 +309,7 @@ CREATE TABLE `province` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -291,6 +318,7 @@ CREATE TABLE `province` (
 
 LOCK TABLES `province` WRITE;
 /*!40000 ALTER TABLE `province` DISABLE KEYS */;
+INSERT INTO `province` VALUES (1,'Aceh'),(2,'Sumatera Utara'),(3,'Sumatera Barat'),(4,'Riau'),(5,'Jambi'),(6,'Sumatera Selatan'),(7,'Bengkulu'),(8,'Lampung'),(9,'Kepulauan Bangka Belitung'),(10,'Kepulauan Riau'),(11,'Dki Jakarta'),(12,'Jawa Barat'),(13,'Jawa Tengah'),(14,'Di Yogyakarta'),(15,'Jawa Timur'),(16,'Banten'),(17,'Bali'),(18,'Nusa Tenggara Barat'),(19,'Nusa Tenggara Timur'),(20,'Kalimantan Barat'),(21,'Kalimantan Tengah'),(22,'Kalimantan Selatan'),(23,'Kalimantan Timur'),(24,'Kalimantan Utara'),(25,'Sulawesi Utara'),(26,'Sulawesi Tengah'),(27,'Sulawesi Selatan'),(28,'Sulawesi Tenggara'),(29,'Gorontalo'),(30,'Sulawesi Barat'),(31,'Maluku'),(32,'Maluku Utara'),(33,'Papua Barat'),(34,'Papua');
 /*!40000 ALTER TABLE `province` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -929,4 +957,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-04 16:54:17
+-- Dump completed on 2019-01-07  4:11:39
